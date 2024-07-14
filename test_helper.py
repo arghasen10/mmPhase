daa596dc90079c1febbfed73d8ed4f12823cf3fd
arghasen10 <arghasen10@gmail.com>
2024-07-14 23:57:13 +0000

    model updated

--- test_helper.py
+++ test_helper.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 import os
 import concurrent.futures
-# from tqdm import tqdm
 from helper import PointCloudProcessCFG, get_velocity, find_peaks_in_range_data
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
 np.random.seed(42)
 tf.random.set_seed(42)
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
@@ -18,7 +20,12 @@ y_est=df['y_estimate'].astype(np.float32)
 
 y_TRUE=np.hstack((y_est.reshape(-1,1),y_true.reshape(-1,1)))
 
-dataset=tf.data.Dataset.from_tensor_slices((rangeResultabs,y_TRUE)).shuffle(512).repeat(-1).batch(64,drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE)
+X_train, X_temp, y_train, y_temp = train_test_split(rangeResultabs, y_TRUE, test_size=0.25, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.6, random_state=42)
+train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).shuffle(512).repeat(-1).batch(64, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE)
+val_dataset = tf.data.Dataset.from_tensor_slices((X_val, y_val)).shuffle(512).repeat(-1).batch(64, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE)
+test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(64).prefetch(tf.data.experimental.AUTOTUNE)
+
 
 def build_cnn2d():
     model2d = tf.keras.Sequential([
@@ -38,7 +45,32 @@ def loss_fn(y_true, y_pred):
     return mse_loss
 
 model = build_cnn2d()
-# optimizer = tf.keras.optimizers.Adam()
 model.compile(loss=loss_fn,optimizer='adam')
+checkpoint = tf.keras.callbacks.ModelCheckpoint('best_model.keras', monitor='val_loss', save_best_only=True, mode='min', verbose=1)
 
-history=model.fit(dataset,epochs=100,steps_per_epoch=512)
+history = model.fit(train_dataset, epochs=1000, steps_per_epoch=len(X_train) // 64, validation_data=val_dataset, validation_steps=len(X_val) // 64, callbacks=[checkpoint])
+
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Loss Curves')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('model_loss.png')
+
+model.load_weights('best_model.keras')
+
+y_pred = model.predict(X_test)
+
+mae_metric = tf.keras.metrics.MeanAbsoluteError()
+mse_metric = tf.keras.metrics.MeanSquaredError()
+
+mae_metric.update_state(y_test[:, 1], y_pred)
+mse_metric.update_state(y_test[:, 1], y_pred)
+
+mae = mae_metric.result().numpy()
+mse = mse_metric.result().numpy()
+
+print(f'MAE: {mae}')
+print(f'MSE: {mse}')
+
